4ce162aa12b39b71701ef0437cc3717c5ab3433e
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-05-19 14:32:23 +0600

    extra tools features schema ready

---
+++ src/models/toilet.model.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+
+const toiletSchema = new mongoose.Schema({ 
+    name: {
+        type: String,
+        required: true
+    },
+    mfg: {
+        type: String,
+        required: true 
+    },
+    modelNumber: {
+        type: String,
+        required: true
+    },
+    BTU: {
+        type: String,
+        required: true
+    }, 
+    dateOfPurchase: {
+        type: String,
+        required: true
+    }, 
+    location: {
+        type: String,
+        required: true,
+        enum: ['Front Left', 'Front Right', 'Rear Left', 'Rear Right', 'Mid Left', 'Mid Right', 'Rear']
+    },
+    price: {    
+        type: Number,
+        required: true
+    },
+    vendor: {
+        type: String,
+        required: true
+    },
+    picture: {
+        type: String,
+        required: true
+    },
+    notes: {
+        type: String,
+        required: true
+    }, 
+    rvId: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'RV',
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+}, {
+    timestamps: true
+});
+
+
+const Toilet = mongoose.model('Toilet', toiletSchema);
+exports = Toilet;
+ 
