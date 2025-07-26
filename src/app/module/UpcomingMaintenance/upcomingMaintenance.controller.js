8d557406bf0777cf9582d90b99f34fd333054b37
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-07-26 18:00:11 +0600

    up maintenance scaleable

--- src/app/module/UpcomingMaintenance/upcomingMaintenance.controller.js
+++ src/app/module/UpcomingMaintenance/upcomingMaintenance.controller.js
@@ -1,28 +1,20 @@
 const ONE_DAY = 1000 * 60 * 60 * 24;
 const ONE_MILE = 1;
+const MAX_DAYS = 7;
+const MAX_MILES = 1;
 
 exports.getUpcomingMaintenance = asyncHandler(async (req, res) => {
     const maintenanceSchedules = await MaintenanceSchedule.find({});
 
-    const upcomingMaintenances = maintenanceSchedules.map(maintenance => {
-        const performDate = new Date(maintenance.maintenanceToBePerformed);
-        const today = new Date();
-        const diffTime = Math.abs(performDate - today);
-        const diffDays = Math.ceil(diffTime / ONE_DAY);
-
-        const { currentMileage } = req.user.rv;
-
-        const diffMilage = Math.abs(currentMileage - maintenance.initialMilage);
-
-        if (diffDays <= 7 || diffMilage <= ONE_MILE) {
-            return {
-                ...maintenance._doc,
-                color: 'red'
-            };
-        }
-
-        return maintenance;
-    });
+    const upcomingMaintenances = maintenanceSchedules
+        .map((maintenance) => ({
+            ...maintenance._doc,
+            daysUntilMaintenance: getDaysUntilMaintenance(maintenance.maintenanceToBePerformed),
+            milesUntilMaintenance: getMilesUntilMaintenance(maintenance.initialMilage, req.user.rv.currentMileage),
+        }))
+        .filter(({ daysUntilMaintenance, milesUntilMaintenance }) => 
+            daysUntilMaintenance <= MAX_DAYS || milesUntilMaintenance <= MAX_MILES
+        );
 
     res.status(200).json({
         success: true,
@@ -31,3 +23,12 @@ exports.getUpcomingMaintenance = asyncHandler(async (req, res) => {
     });
 });
 
+function getDaysUntilMaintenance(date) {
+    const today = new Date();
+    const diffTime = Math.abs(date - today);
+    return Math.ceil(diffTime / ONE_DAY);
+}
+
+function getMilesUntilMaintenance(initialMileage, currentMileage) {
+    return Math.abs(currentMileage - initialMileage);
+}
