d84afeb0bc24cba9faa0b1d5b8fef2b3bd8e09a6
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-09-22 17:06:59 +0600

    hola

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const PORT = process.env.PORT || 5002;
 const HOST = process.env.HOST_URL || '0.0.0.0';
 
 
-// app.listen(PORT, HOST, () => {
-//   console.log(`🚐 RV SaaS backend is running on port http://${HOST}:${PORT}`);
-// });
-
-
-
-app.listen(PORT, () => {
-  console.log(`🚐 RV SaaS backend is running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`🚐 RV SaaS backend is running on port http://${HOST}:${PORT}`);
 });
+
+
+
+// app.listen(PORT, () => {
+//   console.log(`🚐 RV SaaS backend is running on port ${PORT}`);
+// });
