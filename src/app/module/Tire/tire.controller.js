3bd0a8c97c3bd13d0588951c8b30dd02e2bf22de
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-09-22 17:19:08 +0600

    tire issue

--- src/app/module/Tire/tire.controller.js
+++ src/app/module/Tire/tire.controller.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const QueryBuilder = require('../../../builder/queryBuilder');
 const deleteDocumentWithFiles = require('../../../utils/deleteDocumentWithImages');
-
+const getSelectedRvByUserId = require('../../../utils/currentRv')
 const deleteFile = require('../../../utils/unlinkFile');
 
 const uploadPath = path.join(__dirname, '../uploads');
