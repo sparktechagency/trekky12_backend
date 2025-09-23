53e9b57ff718d39007defd5da0d1d3c45be9829e
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-09-23 18:40:11 +0600

    outdoorRadio updated also s3 done

--- src/app/module/OutdoorRadio/outdoorRadio.controller.js
+++ src/app/module/OutdoorRadio/outdoorRadio.controller.js
@@ -1,34 +1,80 @@
 const OutdoorRadio = require('./OutdoorRadio');
 const asyncHandler = require('../../../utils/asyncHandler');
 const { ApiError } = require('../../../errors/errorHandler');
+const fs = require('fs');
+const path = require('path');
+const QueryBuilder = require('../../../builder/queryBuilder');
+const deleteDocumentWithFiles = require('../../../utils/deleteDocumentWithImages');
+const getSelectedRvByUserId = require('../../../utils/currentRv');
 const deleteFile = require('../../../utils/unlinkFile');
+const uploadPath = path.join(__dirname, '../uploads');
 
 exports.createOutdoorRadio = asyncHandler(async (req, res) => {
-    const outdoorRadio = await OutdoorRadio.create(req.body);
+    const userId = req.user.id || req.user._id;
+    const selectedRvId = await getSelectedRvByUserId(userId);
+    let rvId = req.body.rvId;
+    if(!rvId && !selectedRvId) throw new ApiError('No selected RV found', 404);
+    if(!rvId) rvId = selectedRvId;
+    
+    const outdoorRadio = await OutdoorRadio.create({
+        rvId,
+        ...req.body,
+        user: userId,
+    });
+    
+    const images = req.files;
     if (!outdoorRadio) throw new ApiError('OutdoorRadio not created', 500);
 
-    const images = req.files;
-        if (!images) throw new ApiError('No images uploaded', 400);
-    
-        if (images && images.length > 0) {
-            const imagePaths = images.map(image => image.path);
-            outdoorRadio.images = imagePaths;
-            await outdoorRadio.save();
-        }
+    if (images && images.length > 0) {
+        const imagePaths = images.map(image => image.location);
+        outdoorRadio.images = imagePaths;
+        await outdoorRadio.save();
+    }
+
     res.status(201).json({
         success: true,
         message: 'OutdoorRadio created successfully',
         outdoorRadio
     });
-}); 
+});
 
 exports.getOutdoorRadio = asyncHandler(async (req, res) => {
-    const outdoorRadio = await OutdoorRadio.find();
-    if (!outdoorRadio) throw new ApiError('OutdoorRadio not found', 404);
+    const userId = req.user.id || req.user._id;
+    const selectedRvId = await getSelectedRvByUserId(userId);
+    let rvId = req.query.rvId;
+    if(!rvId && !selectedRvId) throw new ApiError('No selected RV found', 404);
+    if(!rvId) rvId = selectedRvId;
+    
+    const baseQuery = { user: userId, rvId };
+    const s = { ...req.query, ...baseQuery };
+
+    const outdoorRadioQuery = new QueryBuilder(
+        OutdoorRadio.find(baseQuery),
+        req.query
+    )
+    
+    const outdoorRadios = await outdoorRadioQuery
+        .search(['name', 'brand'])
+        .filter()
+        .sort()
+        .paginate()
+        .fields()
+        .modelQuery;
+
+    const meta = await new QueryBuilder(
+        OutdoorRadio.find(baseQuery),
+        req.query
+    ).countTotal();
+
+    if (!outdoorRadios || outdoorRadios.length === 0) {
+        throw new ApiError('OutdoorRadios not found', 404);
+    }
+
     return res.status(200).json({
         success: true,
-        message: 'OutdoorRadio retrieved successfully',
-        outdoorRadio
+        message: 'OutdoorRadios retrieved successfully',
+        meta,
+        outdoorRadios
     });
 });
 
@@ -42,18 +88,16 @@ exports.getOutdoorRadioById = asyncHandler(async (req, res) => {
     });
 });
 
-
 exports.updateOutdoorRadio = asyncHandler(async (req, res) => {
     const outdoorRadio = await OutdoorRadio.findById(req.params.id);
     if (!outdoorRadio) throw new ApiError('OutdoorRadio not found', 404);
 
+    // Update outdoorRadio fields from req.body
     Object.keys(req.body).forEach(key => {
         outdoorRadio[key] = req.body[key];
     });
 
     await outdoorRadio.save();
-
-
 
     if (req.files && req.files.length > 0) {
         const oldImages = outdoorRadio.images;
@@ -71,29 +115,10 @@ exports.updateOutdoorRadio = asyncHandler(async (req, res) => {
         });
 
         // Set only new images
-        const newImages = req.files.map(image => image.path.replace('upload/', ''));
+        const newImages = req.files.map(image => image.location);
         outdoorRadio.images = newImages;
+        await outdoorRadio.save();
     }
-
-    // if (req.files && req.files.length > 0) {
-    //     const oldImages = outdoorRadio.images;
-    //     const newImages = req.files.map(image => image.path.replace('upload/', ''));
-    //     outdoorRadio.images = [...oldImages, ...newImages];
-    //     await outdoorRadio.save();
-
-    //     oldImages.forEach(image => {
-    //         const path = image.split('/').pop();
-    //         try {
-    //             deleteFile(`${uploadPath}/${path}`);
-    //         } catch (err) {
-    //             if (err.code !== 'ENOENT') {
-    //                 console.error(err);
-    //             }
-    //         }
-    //     });
-    // } else {
-    //     await outdoorRadio.updateOne(req.body);
-    // }
 
     return res.status(200).json({
         success: true,
@@ -102,26 +127,13 @@ exports.updateOutdoorRadio = asyncHandler(async (req, res) => {
     });
 });
 
-
-// exports.deleteOutdoorRadio = asyncHandler(async (req, res) => {  
-//     const outdoorRadio = await OutdoorRadio.findByIdAndDelete(req.params.id);
-//     if (!outdoorRadio) throw new ApiError('OutdoorRadio not found', 404);
-//     return res.status(200).json({
-//         success: true,
-//         message: 'OutdoorRadio deleted successfully',
-//         outdoorRadio
-//     });
-// });
-
-
-
 exports.deleteOutdoorRadio = asyncHandler(async (req, res) => {
     const outdoorRadio = await deleteDocumentWithFiles(OutdoorRadio, req.params.id, "uploads");
-    if (!outdoorRadio) throw new ApiError("outdoorRadio not found", 404);
+    if (!outdoorRadio) throw new ApiError("OutdoorRadio not found", 404);
 
     return res.status(200).json({
         success: true,
-        message: "outdoorRadio deleted successfully (with images)",
+        message: "OutdoorRadio deleted successfully (with images)",
         outdoorRadio,
     });
 });
