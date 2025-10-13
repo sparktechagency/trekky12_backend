a77edc715c30d78d422bbcd2be30935e57593331
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-10-13 13:14:30 +0600

    cors issue

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = [         // your main frontend from .env
   "http://10.10.20.60:3002",   // fallback localhost
-  "*"
+  "http://localhost:5173"
 ];
 
 
