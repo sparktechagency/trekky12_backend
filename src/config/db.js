c3018e5f06f4d469bab88880ed767d59ca69cb4a
mdshaharulsiyam <shaharulsiyam56@gmail.com>
2025-10-14 22:18:59 -0700

    s

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI,{dbName:"rv-maintainance"});
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection failed:', err.message);
